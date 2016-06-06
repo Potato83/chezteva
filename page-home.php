fe2aa2f298c42ac0529c2390bb343f6754d35e61
Sam Vipond <sammy.potato@gmail.com>
2016-06-06 13:54:21 -0400

    Create Splash Page

--- page-home.php
+++ page-home.php
@@ -6,7 +6,36 @@ get_header(); ?>
 
 <section id="home-page">
 	
-	
+	<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/logo.jpg" alt="logo" class="logo">
+
+	<div class="info-box">
+		<p class="info">5143 Boulevard D&eacute;carie</p>
+		<p class="info">Montr&eacute;al, QC H3W 3C2</p>
+		<p class="info">514 486 5542</p>
+	</div>
+
+	<table>
+			<!-- <th>Heures d'Ouverture</th> -->
+			<!-- <caption>Heures d'Ouverture</caption> -->
+			<tr>
+				<td>L</td>
+				<td>M</td>
+				<td>M</td>
+				<td>J</td>
+				<td>V</td>
+				<td>S</td>
+				<td>D</td>
+			</tr>
+			<tr>
+				<td class="hour">9h - 20h</td>
+				<td class="hour">9h - 20h</td>
+				<td class="hour">9h - 20h</td>
+				<td class="hour">9h - 21h</td>
+				<td class="hour">9h - 21h</td>
+				<td class="hour">9h - 18h</td>
+				<td class="hour">10h - 18h</td>
+			</tr>
+		</table>
 
 </section><!-- #home-page -->
 
