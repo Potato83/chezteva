d0a161b0220364fa13e6a237d5a05ed0e158e57c
Sam Vipond <sammy.potato@gmail.com>
2016-06-06 12:31:43 -0400

    First commit

---
+++ header.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Chez_Teva
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<!-- Normalize -->
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/normalize.css">
+
+<!-- Bootstrap core CSS -->
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/bootstrap.min.css"> 
+
+<!-- Bootstrap theme -->
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/bootstrap-theme.min.css">
+
+<!-- CUSTOM CSS -->
+<link rel="stylesheet" type="text/css" href="<?php bloginfo( 'stylesheet_directory' ); ?>/assets/css/main.css">
+
+<!-- FontAwesome Icons -->
+<link href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+<?php wp_head(); ?>
+<!-- HTML5 shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+
+<body <?php body_class(); ?>>
+
+	<div class="site-content">
---
+++ page-home.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Template Name: Home Page
+ */
+get_header(); ?>
+
+<section id="home-page">
+	
+	
+
+</section><!-- #home-page -->
+
+<?php //get_sidebar(); ?>
+<?php get_footer(); ?>
